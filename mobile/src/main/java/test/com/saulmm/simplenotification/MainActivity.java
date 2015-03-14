5aa1dbf7fb5b67ace64572b241c78111f9323351
Saul M <saulmm2@gmail.com>
2015-03-14 18:11:39 +0100

    cleaned the project and modified the .gitignore

--- mobile/src/main/java/test/com/saulmm/simplenotification/MainActivity.java
+++ mobile/src/main/java/test/com/saulmm/simplenotification/MainActivity.java
@@ -6,8 +6,6 @@ import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 
 
