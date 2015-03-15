e6fd5720b9a1b564f40b422366edb9fb58fa80d6
Saul M <saulmm2@gmail.com>
2015-03-15 13:06:57 +0100

    Added a notification with an action

--- mobile/src/main/java/test/com/saulmm/simplenotification/MainActivity.java
+++ mobile/src/main/java/test/com/saulmm/simplenotification/MainActivity.java
@@ -1,16 +1,27 @@
 package test.com.saulmm.simplenotification;
 
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.graphics.BitmapFactory;
+import android.media.AudioManager;
+import android.media.ToneGenerator;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
+
+import static android.support.v4.app.NotificationCompat.Action;
+import static android.support.v4.app.NotificationCompat.Builder;
+import static android.support.v4.app.NotificationCompat.WearableExtender;
 
 
 public class MainActivity extends ActionBarActivity {
 
     private final static int SIMPLE_NOTIFICATION_ID = 1;
+    private final static int ENRICHED_NOTIFICATION_ID = 1;
+
+    private final static String PLAY_SOUND_ACTION = "play_sound";
 
     private NotificationManagerCompat mNotificationManager;
 
@@ -22,18 +33,49 @@ public class MainActivity extends ActionBarActivity {
         mNotificationManager = NotificationManagerCompat.from(this);
     }
 
+    @Override
+    protected void onNewIntent(Intent intent) {
+
+        super.onNewIntent(intent);
+
+        if (intent.getAction().equals(PLAY_SOUND_ACTION)) {
+
+            final ToneGenerator tg = new ToneGenerator(AudioManager.STREAM_NOTIFICATION, 100);
+            tg.startTone(ToneGenerator.TONE_PROP_BEEP);
+        }
+    }
+
     public void showBasicNotification(View view) {
 
-        NotificationCompat.Builder nBuilder = new NotificationCompat.Builder(this)
+        Builder nBuilder = new Builder(this)
             .setContentText("yum!")
             .setContentTitle("Hungry?")
-            .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.hamburger))
+            .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.hamburger_background))
             .setSmallIcon(R.drawable.ic_android_black_24dp);
 
         mNotificationManager.notify(SIMPLE_NOTIFICATION_ID, nBuilder.build());
     }
 
+
     public void showEnrichedNotification(View view) {
 
+        Intent letMeEatEvent = new Intent(Intent.ACTION_VIEW);
+        letMeEatEvent.setData(Uri.parse("http://www.cakeshautecouture.com/"));
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, letMeEatEvent, 0);
+
+        Action action = new Action.Builder(R.drawable.ic_cake,
+            "Show me cakes", pendingIntent)
+            .build();
+
+        Builder nBuilder = new Builder(this)
+            .setContentText("Cakes are really nice")
+            .setContentTitle("It would be nice to buy a cake")
+            .extend(new WearableExtender().addAction(action))
+            .setSmallIcon(R.drawable.ic_cake)
+            .setLargeIcon(BitmapFactory.decodeResource(getResources(),
+                R.drawable.cake_background));
+
+        mNotificationManager.notify(ENRICHED_NOTIFICATION_ID, nBuilder.build());
     }
 }
