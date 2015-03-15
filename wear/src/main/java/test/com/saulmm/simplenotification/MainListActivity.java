6ec15dbcc09f8ddcde35898be7d9255991ff16ad
Saul M <saulmm2@gmail.com>
2015-03-15 21:05:06 +0100

    Created MainListActivity & activity_main_list layout

---
+++ wear/src/main/java/test/com/saulmm/simplenotification/MainListActivity.java
@@ -0,0 +1,14 @@
+package test.com.saulmm.simplenotification;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainListActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_list);
+    }
+}
