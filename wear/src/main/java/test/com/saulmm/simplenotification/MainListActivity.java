829547b029d6920100a16f86399d4095b948def4
Saul M <saulmm2@gmail.com>
2015-03-16 12:46:10 +0100

    added a simple listview

--- wear/src/main/java/test/com/saulmm/simplenotification/MainListActivity.java
+++ wear/src/main/java/test/com/saulmm/simplenotification/MainListActivity.java
@@ -2,6 +2,7 @@ package test.com.saulmm.simplenotification;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.wearable.view.WearableListView;
 
 public class MainListActivity extends Activity {
 
@@ -10,5 +11,12 @@ public class MainListActivity extends Activity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_list);
+
+        String[] elements = getResources().getStringArray(R.array.ui_items);
+
+        WearableListView listView = (WearableListView) findViewById(
+            R.id.activity_main_list_listview);
+
+        listView.setAdapter(new Adapter(this, elements));
     }
 }
